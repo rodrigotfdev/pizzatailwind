5d1d1bd26a101ecb9f68a44853c59271a7d1e567
Rodrigo <rtf.franca@gmail.com>
2023-12-30 21:50:15 -0300

    Fetching Orders

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
-import Error from './ui/Error'
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
     <header>
-        <Link to='/'>Fast React Pizza Co.</Link>
-        <p>Rodrigo</p>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
+      <p>Rodrigo</p>
     </header>
-  )
+  );
 }
