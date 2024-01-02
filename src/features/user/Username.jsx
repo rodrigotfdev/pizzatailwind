bdda7f531d6f70993de6c3a24b054080b1f631f6
Rodrigo <rtf.franca@gmail.com>
2024-01-02 07:01:03 -0300

    Color and text styling with tailwindCSS

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Username() {
+  return (
+    <div className='text-sm font-semibold'>Rodrigo</div>
+  )
+}
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
 
       <SearchOrder />
-      <p>Rodrigo</p>
+      <Username />
     </header>
   );
 }
