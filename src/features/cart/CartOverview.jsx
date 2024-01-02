b4cc2c8b9bdd36c1ba5d632cea91a46d33e2ebdb
Rodrigo <rtf.franca@gmail.com>
2024-01-02 07:24:35 -0300

    Box model and responsive design

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800  text-stone-200 uppercase">
-      <p className="text-stone-300 font-semibold">
+    <div className="bg-stone-800  text-stone-200 uppercase p-4 sm:px-6 text-sm md:text-base">
+      <p className="text-stone-300 sm:space-x-6 font-semibold space-x-4">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
+    <header className="bg-yellow-500 uppercase p-4 border-b-4 border-stone-200 sm:px-6">
       <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
 
       <SearchOrder />
