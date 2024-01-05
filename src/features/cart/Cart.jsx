e1cb3a35f0c5a729f434833c2d6f698ddb59cdfb
Rodrigo <rtf.franca@gmail.com>
2024-01-05 10:48:49 -0300

    Styling the Cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -30,14 +31,23 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
-     <LinkButton to='/menu'>
-        &larr; Back to menu
-      </LinkButton>
-      <h2>Your cart, %NAME%</h2>
-      <div>
-      <Button to="/order/new" type='primary'>Order pizzas</Button>
-        
+    <div className="py-3 px-4">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+
+      <ul className="divide-y divide-stone-300 border-b mt-3">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+
+        <Button type='secondary'>Clear cart</Button>
+
         <button>Clear cart</button>
       </div>
     </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,7 +7,8 @@ export default function Button({ children, disabled, to, type }) {
 
     const styles = {
       primary: base + ' px-4 py-3 md:px-6 md:py-4',
-      small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+      small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+      secondary: "border-2 border-stone-300 uppercase font-semibold text-stone-600 inline-block tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:text-stone-800 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5"
     }
 
   if (to)
